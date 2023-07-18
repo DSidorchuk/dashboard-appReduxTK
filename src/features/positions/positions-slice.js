c00a9781feb3595ee5ac9e3578302845912e2f47
Daniil <sidorchuk.daniil@gmail.com>
2023-07-18 21:15:36 +0300

    first commit

---
+++ src/features/positions/positions-slice.js
@@ -0,0 +1,24 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const positionsSlice = createSlice({
+   name: 'positions',
+   initialState: [],
+   reducers: {
+      addPositions: (_, action) => action.payload,
+   }
+})
+
+export const {addPositions} = positionsSlice.actions;
+export const positionsReducer = positionsSlice.reducer;
+
+export const selectAllPositions = (state) => state.positions;
+
+export const selectVisiblePositions = (state, filters = []) => {
+  if (filters.length === 0) return state.positions;
+
+  return state.positions.filter(pos => {
+    const posFilters = [].concat(pos.role, pos.level, ...pos.languages, ...pos.tools);
+
+    return filters.every(filter => posFilters.includes(filter))
+  })
+}
